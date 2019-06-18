6cbe9b23e48212dd15b2bb5aa4ac3dd2ad5da5bb
NickUsov <nicky.yalta.ua@gmail.com>
2019-06-18 16:49:02 +0300

    01wpf

---
+++ 01_WPF_Controls_1/01_WPF_Controls_1/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _01_WPF_Controls_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Button bt1 = new Button();
+            bt1.Content = "C#";
+            bt1.Height = 20;
+            bt1.Width = 40;
+            bt1.Foreground = Brushes.Green;
+            grid.Children.Add(bt1);
+        }
+    }
+}
